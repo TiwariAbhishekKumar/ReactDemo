a6770012db4078255c829c5598b70ab4f6136de5
UWPTeam@hclsolutiongroup7outlook.onmicrosoft.com <UWPTeam@hclsolutiongroup7outlook.onmicrosoft.com>
2019-02-07 14:57:30 +0530

    new component in React

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Home from './Home';
+
 import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
            
-            Welcome! First react App In VS Code......
+      //       Welcome! First react App In VS Code......
             
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <Router>
+      <div>
+          <h1>Simple SPA</h1>
+          <ul className="header">
+            <li><a href="/Home">Home</a></li>
+            <li><a href="/stuff">Stuff</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+
+          <Route exact path="/Home" component={Home} />
+      {/* <Route path="/about" component={Stuff} />
+      <Route path="/topics" component={Contact} /> */}
+          <div className="content">
+             
+          </div>
+        </div>
+        </Router>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+      <h2>Hello Abhishek! Welcome in component demo</h2>
+    </div>
+  );
+
+export default Home;
