ed1ce1136a7eaa2ec21981dba59d6677cd7a042e
UWPTeam@hclsolutiongroup7outlook.onmicrosoft.com <UWPTeam@hclsolutiongroup7outlook.onmicrosoft.com>
2019-02-08 12:10:30 +0530

    component demo

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const Home = () => (
-    <div>
-      <h2>Hello Abhishek! Welcome in component demo</h2>
-    </div>
-  );
 
+
+// const Home = () => (
+//     <div className='clsdiv'>
+//       <h2>Hello Abhishek! Welcome in component demo</h2>
+//     </div>
+   
+//   );
+
+
+const Home = () => {
+    return React.createElement('div', { id: 'divid', className:'clsdiv'},
+    React.createElement('h1',null, 'Hello Abhishek! Welcome in component demo')
+   );
+
+}
+  
 export default Home;
